6408113de1b10350035b73d311d41bdedcb07066
stanpai <stanpai78@gmail.com>
2025-05-10 00:04:41 +0800

    fix(extractor): Handle null type in logging for better debugging

--- NemoPlay/src/main/kotlin/com/Phisher98/StreamPlayExtractor.kt
+++ NemoPlay/src/main/kotlin/com/Phisher98/StreamPlayExtractor.kt
@@ -883,7 +883,7 @@ object StreamPlayExtractor : StreamPlay() {
             if (animePaheDone == null || animePaheDone!!.isCompleted) animePaheDone = CompletableDeferred()
         }
 
-        Log.d("nemo", type)
+        Log.d("nemo", type ?: "null")
 
         runAllAsync(
             // { malId?.let { invokeAnimeKai(jptitle,zorotitle,it, episode, subtitleCallback, callback, type) } },
